58c805582c34c653ae5dd4a60a20d7a4afd6d2d2
İlyas <ashnwor@gmail.com>
2020-06-22 20:34:54 +0300

    misc: edit site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Airtable Powered Static Site`,
+    description: `This project gets data from Airtable and generates a static site`,
     author: `@gatsbyjs`,
   },
   plugins: [
